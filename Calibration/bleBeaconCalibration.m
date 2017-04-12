0c24640a8d0839b5728f47fb438385e382258db9
Mitch Phillips <phillm4@rpi.edu>
2017-04-12 08:49:35 -0400

    update

--- Calibration/bleBeaconCalibration.m
+++ Calibration/bleBeaconCalibration.m
@@ -13,37 +13,88 @@ clc, clear, close all;
 %% Import RSSI Data for each of the Beacons
 %
 % There are four beacons being used which all share the same UUID, but have
-% different minor numbers. 
+% different minor numbers.
 %
 % Need to sift through data to get only beacons and info of interest.
 %
+% In future just save data to CSV. Parcing data like this is wasteful.
 
-for b = 1:4;
+RSSI_d = zeros(8,4); % downscale RSSI values
+RSSI_u = zeros(8,4); % upscale RSSI values
 
-% beacon1
-addpath('beacon1/');
-
- b1_d = cell(1,8);
- b1_RSSI_d = zeros(8,1);
-% downscale
-for i = 1:8;
-    trial = num2str(5*i -1, '%02d');
-    b1_d{:,i} = importdata(['b1_',trial,'in_d.txt']);
-    b1_RSSI_d(i) = median(bleRSSI(b1_d{:,i}));
+for j = 1:4;
+    
+    b = num2str(j);
+    %
+    addpath(['beacon',b,'/']);
+    
+    b_d = cell(1,8);
+    %b1_RSSI_d = zeros(8,1);
+    % downscale
+    for i = 1:8;
+        trial = num2str(5*i -1, '%02d');
+        b_d{:,i} = importdata(['b',b,'_',trial,'in_d.txt']);
+        RSSI_d(i,j) = median(bleRSSI(b_d{:,i}));
+    end
+    clear i trial
+    
+    b_u = cell(1,8);
+    %b1_RSSI_u = zeros(8,1);
+    % upscale
+    for i = 1:8;
+        trial = num2str(5*i -1, '%02u');
+        b_u{:,i} = importdata(['b',b,'_',trial,'in_u.txt']);
+        RSSI_u(i,j) = median(bleRSSI(b_u{:,i}));
+    end
+    clear i trial b_d b_u
+    
 end
-clear trial b1_d i
 
- b1_u = cell(1,8);
- b1_RSSI_u = zeros(8,1);
-% upscale
-for i = 1:8;
-    trial = num2str(5*i -1, '%02u');
-    b1_u{:,i} = importdata(['b1_',trial,'in_u.txt']);
-    b1_RSSI_u(i) = median(bleRSSI(b1_u{:,i}));
+clear b j
+%% Plot Up and DownScale for Each
+
+dist = 4:5:39; % recorded measurement [in]
+dist = dist.*0.0254; % recorded measurement [m]
+
+figure(1);
+C = {'k','b','r','g'};
+for j = 1:4;
+    plot(dist, RSSI_d(:,j),'-*','color',C{j})
+    hold on
+    plot(dist, RSSI_u(:,j),'--o','color',C{j})
 end
-clear trial b1_u
+clear j
+legend('b1-upscale','b1-downscale','b2-upscale','b2-downscale',...
+    'b3-upscale','b3-downscale','b4-upscale','b4-downscale')
+title('Beacon: RSSI vs. Distance')
+xlabel('Distance [m]')
+ylabel('RSSI [dBm]')
 
-end
+%% Curve Fitting
 
+% average RSSI values 
+RSSI_avg = mean([mean(RSSI_u,2),mean(RSSI_d,2)],2);
 
+% figure(3)
+% loglog(dist, RSSI_avg, 'b*')
+% p = polyfit
+% hold on
+% % [population2, gof] = fit( dist', RSSI_avg,  'poly1' );
+% % plot( population2 )
+% hold off
 
+% parameter tuning
+A = 62;
+n = 1.40;
+d = 0:0.01:1;
+y = -(10*n)*log(d)-A;
+
+figure(2)
+plot(dist, RSSI_avg, 'b*')
+hold on
+plot(d,y,'r')
+hold off
+title('Average Beacon Reference')
+xlabel('Distance [m]')
+ylabel('RSSI [dBm]')
+legend('Average Beacon RSSI', 'Reference Curve')
